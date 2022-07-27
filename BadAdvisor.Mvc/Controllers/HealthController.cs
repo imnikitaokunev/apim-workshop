6624c0dad205f1f293bd5cc22454c2148eb79b1e
Artem Mikulich <artemmikulich@coherentsolutions.com>
2022-07-27 11:12:12 +0300

    HealthController

---
+++ BadAdvisor.Mvc/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BadAdvisor.Mvc.Controllers
+{
+    [Route("health")]
+    public class HealthController: Controller
+    {
+        [HttpGet("live")]
+        public async Task<IActionResult> Live()
+        {
+            return await Task.FromResult(Ok("Bad Advisor is healthy"));
+        }
+    }
+}
